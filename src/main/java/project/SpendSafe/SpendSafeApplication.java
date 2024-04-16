58cf1927b9adde25c4e98fda7a47e6d0ea3eb652
Jagruti Goswami <jagrutigoswami402@gmail.com>
2024-04-16 13:21:09 +0530

    initial commit

---
+++ src/main/java/project/SpendSafe/SpendSafeApplication.java
@@ -0,0 +1,13 @@
+package project.SpendSafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpendSafeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpendSafeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/project/SpendSafe/SpendSafeApplicationTests.java
@@ -0,0 +1,13 @@
+package project.SpendSafe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpendSafeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
